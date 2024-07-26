1e38bc0db762af01a2e6f1e9029c6304483f3939
Daniel Sanguino <153718537+danisanguino@users.noreply.github.com>
2024-07-26 19:21:17 +0200

    add modal contact and fixed header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,26 +1,38 @@
+import { useState } from "react";
 import "./header.css";
+import Modal from "../modal";
+
 
 
 export const Header = () => {
+
+
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
   return (
-    
+    <>
+      <Modal show={showModal} onClose={toggleModal}/>
       <main className="header-main">
         <a href="https://www.linkedin.com/in/dani-sanguino/" target="_blank">
           <button className="header-main__linkedin">
-            <img src="./public/assets/linkedin-icon.svg" className="header-main__linkedin-icon"/>
+            <img src="./public/assets/linkedin-icon.svg" className="header-main__linkedin--icon"/>
           </button>
         </a>
         <a href="https://github.com/danisanguino" target="_blank">
           <button className="header-main__github">
-            <img src="./public/assets/github-icon.svg" className="header-main__github-icon"/>
+            <img src="./public/assets/github-icon.svg" className="header-main__github--icon"/>
           </button>
         </a>
-        <a href="#" target="_blank">
-          <button className="header-main__contact">
+        
+          <button onClick={toggleModal} className="header-main__contact">
             <p>Contact</p>
           </button>
-        </a>
+        
       </main>
-
+    </>
   )
 }
---
+++ src/components/modal/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import "./modal.css"
+import {IoCloseCircle, IoMail } from "react-icons/io5"
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
+
+
+interface ModalProps {
+    show: boolean;
+    onClose: () => void;
+    phoneNumber: string;
+    message: string;
+  }
+
+
+  const Modal: React.FC<ModalProps> = ({ show, onClose, phoneNumber, message  }) => {
+    if (!show) {
+      return null;
+    }
+
+    const handleWp = () => {
+        const url = `https://wa.me/${656588717}?text=${encodeURIComponent("Si quieres escribirme estaré encantado de charlar contigo")}`;
+        window.open(url, '_blank');
+    };
+  
+    return (
+      <div className="modal-overlay" onClick={onClose}>
+        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+          
+            <IoCloseCircle size={40} onClick={onClose} className="modal-close" />
+            <div className='modal-content__data'>
+                <FaPhoneAlt className='modal-content__data--modal-icon'/>
+                <IoLogoWhatsapp style={{cursor: 'pointer'}} onClick={handleWp} className='modal-content__data--modal-icon' />
+                <span> +34 656 588 717</span>
+            </div>
+            <div className='modal-content__data'>
+                <IoMail className='modal-content__data--modal-icon' /><span>danielsanguino1980@gmail.com</span>
+            </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Modal;
