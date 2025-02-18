18bb1a77964ef02ea0a20647625212fc81685010
Daniel Sanguino <153718537+danisanguino@users.noreply.github.com>
2025-02-18 18:46:17 +0100

    update resume and year

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,7 +3,7 @@ import "./footer.css"
 export const Footer = () => {
   return (
     <main className="footer-main">
-        <p>Dani Sanguino | 2024© All Rights Reserved</p>
+        <p>Dani Sanguino | 2025© All Rights Reserved</p>
     </main>
   )
 }
